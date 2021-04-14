02846102d650b0450c58398df0513286ba658b1a
momopeachyh <76701058+momopeachyh@users.noreply.github.com>
2021-04-14 18:20:32 -0500

    Added bootstrap, footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <Dictionary />
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer text-center">
+      <a href="https://github.com/momopeachyh/dictionary-app">
+        Open-source code
+      </a>{" "}
+      by Heidi Willden
+    </div>
+  );
+}
