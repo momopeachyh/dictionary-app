0316fdab73f7172b37a2fae8ba1ad9910efa388d
momopeachyh <76701058+momopeachyh@users.noreply.github.com>
2021-04-14 19:53:12 -0500

    Created search bar

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary">hello</div>;
+  let [searchterm, setSearchterm] = useState(null);
+
+  function handleSearchChange(event) {
+    setSearchterm(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(searchterm);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          onChange={handleSearchChange}
+          placeholder="Enter your favorite word!"
+        />
+        <input type="submit" value="search" />{" "}
+      </form>
+    </div>
+  );
 }
