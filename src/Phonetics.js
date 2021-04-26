03e5f96ba0b1fa2c203ab136054bbb4b7c44d478
momopeachyh <76701058+momopeachyh@users.noreply.github.com>
2021-04-26 18:24:50 -0500

    Updated styling, added audio integration to pronunciation

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,5 +1,22 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  return <div className="Phonetics">{props.phonetics.text}</div>;
+  function playPronunciation(event) {
+    event.preventDefault();
+    let Pronunciation = new Audio(props.phonetics.audio);
+    Pronunciation.play();
+  }
+
+  return (
+    <div className="Phonetics">
+      <span>
+        <button onClick={playPronunciation}>
+          <i className="fas fa-volume-up"></i>
+        </button>{" "}
+        {""}
+        {props.phonetics.text}
+      </span>
+    </div>
+  );
 }
